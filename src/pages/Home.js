3b6050a66b80a5155f66e16a9ccf1fcb190c2fd5
Supriya Dua <sda92@sfu.ca>
2023-10-23 21:27:36 -0700

    header footer home page

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import {Link} from "react-router-dom";
+import BannerImage from '../assets/food.jpg'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})`}}>
+      <div className="headerContainer" >
+        <h1>Generic Food Website</h1>
+        <p> FOOD TO FIT ANY TASTE </p>
+        <Link to="/menu">
+        <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='menu'>
+      <h3 className='menuTitle'> Our Menu </h3>
+      <div className='menuList'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Menu
